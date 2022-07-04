c6d928d3c8e575b8e66f29c62a54d95b104721f5
rangulvers <malte.sohns@googlemail.com>
2022-07-04 18:09:15 +0200

    add unicode table

--- app.py
+++ app.py
@@ -8,26 +8,44 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         lang = request.form["language"]
-        pw, shortcodes = generatePW(request.form["inputEmoji"], lang)
-        return render_template("index.html", result=pw, em=request.form["inputEmoji"], shortcodes=shortcodes)
+        pw, metadata = generatePW(request.form["inputEmoji"], lang)
+        return render_template("index.html", result=pw, em=request.form["inputEmoji"], metadata=metadata)
     elif request.method == 'GET':
         return render_template('index.html')
 
 
 def generatePW(inputEmoji, lang):
-    lst = [str.upper, str.lower]
+
     newPW = []
+    metadata = []
     shortcodes = []
     for e in inputEmoji:
-        shortcode = getShortCode(e, lang)
-        if shortcode.startswith(":"):
+        if emoji.is_emoji(e):
+            shortcode = getShortCode(e, lang)
             shortcodes.append(shortcode)
+            unicode = emoji.replace_emoji(e, replace=unicode_escape)
+            meta = {"emoji": e, "short": shortcode,
+                    "unicode_clean": clean_unicode(unicode), "unicode": unicode, "lang": lang}
+            metadata.append(meta)
             newPW.append(random.choice(shortcode))
-    return ''.join(random.choice(lst)(c) for c in newPW), shortcodes
+    return upperLower(newPW), metadata
 
 
 def getShortCode(input, lang):
     return emoji.demojize(input, language=lang)
+
+
+def unicode_escape(chars, data_dict):
+    return chars.encode('unicode-escape').decode()
+
+
+def clean_unicode(code):
+    return code[2:].lstrip("0")
+
+
+def upperLower(pw):
+    lst = [str.upper, str.lower]
+    return ''.join(random.choice(lst)(c) for c in pw)
 
 
 if __name__ == '__main__':
