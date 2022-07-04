e88cc85cf7711101c68fb7d47851d0ef8c1bf850
rangulvers <malte.sohns@googlemail.com>
2022-07-04 11:38:36 +0200

    procfile

--- app.py
+++ app.py
@@ -24,3 +24,8 @@ def generatePW(inputEmoji):
             newPWF = ''.join(random.choice(lst)(c) for c in newPwSt)
     print(newPWF)
     return newPWF
+
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
